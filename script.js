4faa33b2d25ce6ee10937cdc905487ecc07c59cd
Jevinde <152850208+Jevinde@users.noreply.github.com>
2023-12-04 15:16:51 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const wrapper document.querySelector(".wrapper");
+const question document.querySelector(".question"); const gif document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+yesBtn.addEventListener("click", () {
+question.innerHTML = "Aaaaa, I like you too";
+
+gif.src = "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp"; });
+noBtn.addEventListener("mouseover", () {
+const noßtnRect = noßtn.getBoundingClientRect();
+const maxX = window.innerWidthnoBtnRect.width;
+const maxy = window.innerHeight noßtnRect.height;
+const randomX = Math.floor(Math.random() maxX);
+const randomy Math.floor(Math.random()*maxY);
+noBtn.style.left = randomX + "px"; noBtn.style.top = randomy + "px"; });
